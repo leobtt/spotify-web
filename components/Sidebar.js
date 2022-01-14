b5206db47e9dda40c82eba71e388498622ff0a39
leobtt <leonarbat8@gmail.com>
2022-01-13 23:57:46 -0300

    init

---
+++ components/Sidebar.js
@@ -0,0 +1,12 @@
+import { HomeIcon } from '@heroicons/react/outline'
+
+const Sidebar = () => {
+  return (
+    <div>
+      <HomeIcon className="h-5 w-5" />
+      <h1 className="uppercase">This is sidebar</h1>
+    </div>
+  )
+}
+
+export default Sidebar
