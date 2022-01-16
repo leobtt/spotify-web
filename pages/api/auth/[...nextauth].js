69db70e47987466d6a009af494cb45a064a2f854
leobtt <leonarbat8@gmail.com>
2022-01-16 12:09:49 -0300

    token

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,30 @@
 import NextAuth from 'next-auth'
 import SpotifyProvider from 'next-auth/providers/spotify'
-import { LOGIN_URL } from '../../../lib/spotify'
+import spotifyApi, { LOGIN_URL } from '../../../lib/spotify'
+
+const refreshAccessToken = async (token) => {
+  try {
+    spotifyApi.setAccessToken(token.accessToken)
+    spotifyApi.setRefreshToken(token.refreshToken)
+
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken()
+    console.log('O toke atualizado é ', refreshedToken)
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now + refreshedToken.expires_in * 1000,
+      // replace if new one came back else fall back to old refreshed token
+      refreshedToken: refreshedToken.refresh_token ?? token.refreshToken,
+    }
+  } catch (err) {
+    console.log(err)
+    return {
+      ...token,
+      error: 'RefreshAccessTokenError',
+    }
+  }
+}
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -12,4 +36,41 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  secret: process.env.JWT_SECRET,
+  pages: {
+    singIn: '/login',
+  },
+  callbacks: {
+    async jwt({ token, account, user }) {
+      // initial sign in
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000,
+        }
+      }
+
+      // return de previous token if the access token has not expired yet
+      if (Date.now() < token.accessTokenExpires) {
+        console.log('O token é válido')
+        return token
+      }
+
+      // Access token expires has expired, so we need to refresh it
+      console.log('O token expirou, atualizando...')
+      return await refreshAccessToken(token)
+    },
+
+    // create a session for user
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken
+      session.user.refreshToken = token.refreshToken
+      session.user.username = token.username
+
+      return session
+    },
+  },
 })
