2ac6a19ebc4ebdbd4d27e61f2a5376fe9451f4bc
leobtt <leonarbat8@gmail.com>
2022-01-19 23:37:18 -0300

    exit icon/ test

--- components/sidebar/Sidebar.js
+++ components/sidebar/Sidebar.js
@@ -5,14 +5,19 @@ import {
   PlusCircleIcon,
   HeartIcon,
   RssIcon,
+  LogoutIcon,
 } from '@heroicons/react/outline'
 import Button from './Button'
+import { signOut, useSession } from 'next-auth/react'
 
 const Sidebar = () => {
+  const { data: session, status } = useSession()
+  console.log(session)
   return (
     <>
       {/* Menu */}
       <div className="text-gray-500 p-5 text-sm border-r border-gray-900 space-y-4">
+        <Button name="Sair" Icon={LogoutIcon} click={() => signOut()} />
         <Button name="Ínicio" Icon={HomeIcon} />
         <Button name="Buscar" Icon={SearchIcon} />
         <Button name="Sua Biblioteca" Icon={LibraryIcon} />
@@ -23,6 +28,7 @@ const Sidebar = () => {
         <Button name="Seus Episódios" Icon={RssIcon} />
         <hr className="border-t-[1px] border-gray-900" />
 
+        {/* Playlist */}
         <p className="cursor-pointer hover:text-white">Playlist name...</p>
         <p className="cursor-pointer hover:text-white">Playlist name...</p>
         <p className="cursor-pointer hover:text-white">Playlist name...</p>
@@ -35,7 +41,6 @@ const Sidebar = () => {
         <p className="cursor-pointer hover:text-white">Playlist name...</p>
         <p className="cursor-pointer hover:text-white">Playlist name...</p>
       </div>
-      {/* Playlist */}
     </>
   )
 }
