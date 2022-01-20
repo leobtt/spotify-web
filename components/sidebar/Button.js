e4c12a54670310f9a53fe8abf8040468046157bf
leobtt <leonarbat8@gmail.com>
2022-01-19 23:36:36 -0300

    add click event

--- components/sidebar/Button.js
+++ components/sidebar/Button.js
@@ -1,6 +1,9 @@
-const Button = ({ Icon, name }) => {
+const Button = ({ Icon, name, click }) => {
   return (
-    <button className="flex items-center space-x-2 hover:text-green-500">
+    <button
+      className="flex items-center space-x-2 hover:text-green-500"
+      onClick={click}
+    >
       <Icon className="h-5 w-5" />
       <p>{name}</p>
     </button>
