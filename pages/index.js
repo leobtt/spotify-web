db7ea8e7a6f0e741d54d9ff1382cbfcbcdf3d807
leobtt <leonarbat8@gmail.com>
2022-01-21 00:37:06 -0300

    center

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Sidebar from '../components/sidebar/Sidebar'
+import Center from '../components/Center'
 
 export default function Home() {
   return (
     <div className="bg-black h-screen overflow-hidden">
-      <main>
+      <main className="flex">
         <Sidebar />
-        {/* Center */}
+        <Center />
       </main>
 
       {/* Player */}
