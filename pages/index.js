1e0841c52fc550a086a8e1b01facc7422ef17e52
leobtt <leonarbat8@gmail.com>
2022-01-22 00:55:26 -0300

    solving the first render

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Sidebar from '../components/sidebar/Sidebar'
 import Center from '../components/Center'
+import { getSession } from 'next-auth/react'
 
 export default function Home() {
   return (
@@ -12,4 +13,14 @@ export default function Home() {
       {/* Player */}
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+
+  return {
+    props: {
+      session,
+    },
+  }
 }
