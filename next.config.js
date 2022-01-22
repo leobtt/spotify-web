d00abd2b23c1afc7a6be2fb0700c7ebc4eef49b8
leobtt <leonarbat8@gmail.com>
2022-01-22 00:54:37 -0300

    domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['scontent-atl3-2.xx.fbcdn.net', 'scontent-ort2-1.xx.fbcdn.net'],
+    domains: [
+      'scontent-atl3-2.xx.fbcdn.net',
+      'scontent-ort2-1.xx.fbcdn.net',
+      'i.scdn.co',
+      'mosaic.scdn.co',
+    ],
   },
 }
