609e124a4af7308645c8e18f64b86ac6911132a4
leobtt <leonarbat8@gmail.com>
2022-01-21 22:05:23 -0300

    random colors

--- components/Center.js
+++ components/Center.js
@@ -1,23 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSession } from 'next-auth/react'
 import Image from 'next/image'
+import { ChevronDownIcon } from '@heroicons/react/outline'
+import { shuffle } from 'lodash'
+
+const colors = [
+  'from-indigo-500',
+  'from-blue-500',
+  'from-green-500',
+  'from-red-500',
+  'from-yellow-500',
+  'from-pink-500',
+  'from-purple-500',
+]
 
 const Center = () => {
   const { data: session } = useSession()
-  console.log(JSON.stringify(session?.user, null, 2))
+  const [color, setColor] = useState(null)
+
+  console.log(session)
+
+  useEffect(() => {
+    setColor(shuffle(colors).pop())
+  }, [])
   return (
-    <div className="flex flex-grow">
-      <header>
-        {session?.user && (
-          <Image
-            src={session?.user.image}
-            alt="user-profile"
-            width={50}
-            height={50}
-            className="relative rounded-full border-8 border-green-200"
-          />
-        )}
+    <div className="flex-grow">
+      <header className="absolute top-3 right-4">
+        <div className="flex items-center space-x-3 rounded-full p-[2px] pr-2 bg-red-200 opacity-90 hover:opacity-80 cursor-pointer">
+          <div className=" rounded-full w-[40px] h-[40px]">
+            {session?.user && (
+              <Image
+                src={session.user.image}
+                alt={session.user.name}
+                width={40}
+                height={40}
+                className="rounded-full"
+              />
+            )}
+          </div>
+          <h2 className="text-white">{session?.user.name}</h2>
+          <ChevronDownIcon className="h-5 w-5 tex" />
+        </div>
       </header>
+      <section
+        className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8`}
+      >
+        <h1>hello</h1>
+      </section>
     </div>
   )
 }
